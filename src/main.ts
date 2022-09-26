9e984558fe70a9233bcee5f7c39efc16e4bfcadd
ayocodingit <ayocodingit@gmail.com>
2022-09-26 09:53:21 +0700

    common: validation with joi

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { AppConfigService } from './config/app/config.service';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const appConfig: AppConfigService = app.get(AppConfigService);
-  app.useGlobalPipes(new ValidationPipe());
   await app.listen(appConfig.port);
 }
 bootstrap();
