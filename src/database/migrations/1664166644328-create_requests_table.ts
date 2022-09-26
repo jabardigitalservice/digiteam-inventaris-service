486cacf6c10e189aecb60ad7119f0a230c04f579
iqbal167 <muhammadiqbalali167@gmail.com>
2022-09-26 14:41:16 +0700

    change migration flow to first

---
+++ src/database/migrations/1664166644328-create_requests_table.ts
@@ -0,0 +1,79 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateRequestTable1664166644328 implements MigrationInterface {
+  private tableName = 'requests';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tableName,
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            length: '36',
+            isPrimary: true,
+          },
+          {
+            name: 'request_type',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'user_name',
+            type: 'varchar',
+            length: '255',
+            isNullable: true,
+          },
+          {
+            name: 'item_name',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'purpose',
+            type: 'text',
+            isNullable: false,
+          },
+          {
+            name: 'priority',
+            type: 'int',
+            isNullable: false,
+          },
+          {
+            name: 'status',
+            type: 'int',
+            isNullable: false,
+            default: 1,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp(6)',
+            default: 'CURRENT_TIMESTAMP(6)',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp(6)',
+            default: 'CURRENT_TIMESTAMP(6)',
+            onUpdate: 'CURRENT_TIMESTAMP(6)',
+            isNullable: false,
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp(6)',
+            default: 'null',
+            isNullable: true,
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE ${this.tableName}`);
+  }
+}
