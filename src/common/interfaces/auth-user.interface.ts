f034ace39ea0126038bfb32cfb03fc8e6c334678
iqbal167 <muhammadiqbalali167@gmail.com>
2022-10-03 11:26:15 +0700

    create user access from auth

--- src/common/interfaces/auth-user.interface.ts
+++ src/common/interfaces/auth-user.interface.ts
@@ -1,5 +1,6 @@
 export interface AuthUser {
   realm_access: RealmAccess;
+  resource_access: ResourceAccess;
   email_verified: boolean;
   name: string;
   preferred_username: string;
@@ -11,3 +12,32 @@ export interface AuthUser {
 export interface RealmAccess {
   roles: string[];
 }
+
+export interface ResourceAccess {
+  'digiteam-inventaris-app': RealmAccess;
+  account: RealmAccess;
+  client: RealmAccess;
+}
+
+export interface UserAccess {
+  name: string;
+  email: string;
+  role: string[];
+  isAdmin: boolean;
+}
+
+export const getUserAccess = (authUser: AuthUser): UserAccess => {
+  const role = authUser.resource_access['digiteam-inventaris-app'].roles.map(
+    (role) => role,
+  );
+
+  const isAdmin =
+    authUser.resource_access['digiteam-inventaris-app'].roles.includes('admin');
+
+  return {
+    name: authUser.name,
+    email: authUser.email,
+    role: role,
+    isAdmin: isAdmin,
+  };
+};
