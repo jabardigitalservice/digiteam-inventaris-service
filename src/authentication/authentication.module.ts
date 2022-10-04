b024ff26ac397a92b647e66b06d6015fcea7fd41
iqbal167 <muhammadiqbalali167@gmail.com>
2022-10-04 16:04:56 +0700

    removing autentication module to avoid ambiguity between authentication and authorization

--- src/authentication/authentication.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RoleAccessService } from '../common/providers/role-access.service';
-import { AuthenticationService } from './authentication.service';
-@Module({
-  providers: [AuthenticationService, RoleAccessService],
-  exports: [AuthenticationService],
-})
-export class AuthenticationModule {}
