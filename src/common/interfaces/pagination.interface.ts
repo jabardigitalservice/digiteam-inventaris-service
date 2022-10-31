67480722121f55d3295d8a67dc2caed1d4cd4b61
iqbal167 <muhammadiqbalali167@gmail.com>
2022-10-31 10:29:52 +0700

    add: sort_by with  direction key

--- src/common/interfaces/pagination.interface.ts
+++ src/common/interfaces/pagination.interface.ts
@@ -1,13 +1,18 @@
 export interface QueryPagination {
   page: number;
   limit: number;
+  sort_by?: string;
+  sort?: 'asc' | 'desc' | 'ASC' | 'DESC';
 }
 
 export interface Pagination {
   page: number;
   limit: number;
   offset: number;
+  sort_by?: string;
+  sort?: 'asc' | 'desc' | 'ASC' | 'DESC';
 }
+
 export interface MetaPagination {
   page: number;
   limit: number;
