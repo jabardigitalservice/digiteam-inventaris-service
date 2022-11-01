804595a77846af773603c32921186abcd1db7c0a
ayocodingit <ayocodingit@gmail.com>
2022-11-01 07:53:57 +0700

    setup new relic for apm

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,16 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { NewrelicInterceptor } from './common/interceptors/newrelic.interceptor';
+import logger from './common/logger/winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, logger);
 
   const configService: ConfigService = app.get(ConfigService);
   app.enableCors();
   app.use(helmet());
+  app.useGlobalInterceptors(new NewrelicInterceptor());
 
   await app.listen(configService.get('app.port'));
 }
