bdcfad722d1578b09f518e9e7d3ba6f0693b7dc1
iqbal167 <muhammadiqbalali167@gmail.com>
2022-11-03 12:30:46 +0700

    fix: missing minio client module

--- src/modules/requests/requests.module.ts
+++ src/modules/requests/requests.module.ts
@@ -7,9 +7,10 @@ import { Request } from '../../entities/request.entity';
 import { RequestsRepository } from './requests.repository';
 import { UserAccessService } from '../../common/providers/user-access.service';
 import { RoleAccessService } from '../../common/providers/role-access.service';
+import { MinioClientModule } from '../../storage/minio/minio.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Request])],
+  imports: [MinioClientModule, TypeOrmModule.forFeature([Request])],
   providers: [
     ConfigService,
     RequestsRepository,
