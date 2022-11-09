78283e499cfafa9b8187132c705dae57ea4661e1
iqbal167 <muhammadiqbalali167@gmail.com>
2022-11-09 09:24:35 +0700

    add findall options interface

--- src/modules/requests/requests.interface.ts
+++ src/modules/requests/requests.interface.ts
@@ -22,11 +22,17 @@ export interface Update {
 }
 
 export interface FindAll {
+  email?: string;
+  isAdmin?: boolean;
   page?: number;
   limit?: number;
+  offset?: number;
   sort_by?: string;
   sort?: 'asc' | 'desc';
   request_type?: number;
   division?: string;
   status?: number;
+  q?: string;
+  username?: string;
+  phone_number?: string;
 }
