d96d9fba2d42fcda1f640091b8ee407944cee236
iqbal167 <muhammadiqbalali167@gmail.com>
2022-11-10 10:49:01 +0700

    refactor update payload schema

--- src/modules/requests/requests.rules.ts
+++ src/modules/requests/requests.rules.ts
@@ -26,27 +26,46 @@ export const FindAllPayloadSchema = Joi.object({
   q: Joi.string().allow(...emptyAllow),
 });
 
-export const UpdateStatusPayloadSchema = Joi.object({
-  status: Joi.number().strict().required(),
-});
-
-export const UpdateItemPayloadSchema = Joi.object({
-  item_name: Joi.string().required(),
-  item_brand: Joi.string().required(),
-  item_number: Joi.string().required(),
-});
-
-export const UpdateNotesPayloadSchema = Joi.object({
-  status: Joi.number().strict().equal(status.REJECTED, status.READY).required(),
-  notes: Joi.string().required(),
-});
-
-export const UpdatePickupPayloadSchema = Joi.object({
-  pickup_signing: Joi.string().required(),
-  pickup_evidence: Joi.string().required(),
-  pickup_bast: Joi.string().required(),
-});
-
-export const UpdateFilenamePayloadSchema = Joi.object({
-  filename: Joi.string().required(),
+export const UpdatePayloadSchema = Joi.object({
+  status: Joi.number().strict().equal(),
+  notes: Joi.alternatives().conditional('status', {
+    is: Joi.equal(status.REJECTED, status.CHECKED),
+    then: Joi.string().required(),
+    otherwise: Joi.forbidden(),
+  }),
+  filename: Joi.alternatives().conditional('status', {
+    is: Joi.equal(status.APPROVED),
+    then: Joi.string().required(),
+    otherwise: Joi.forbidden(),
+  }),
+  item_name: Joi.alternatives().conditional('status', {
+    is: Joi.equal(status.REQUESTED),
+    then: Joi.string().required(),
+    otherwise: Joi.forbidden(),
+  }),
+  item_brand: Joi.alternatives().conditional('status', {
+    is: Joi.equal(status.REQUESTED),
+    then: Joi.string().required(),
+    otherwise: Joi.forbidden(),
+  }),
+  item_number: Joi.alternatives().conditional('status', {
+    is: Joi.equal(status.REQUESTED),
+    then: Joi.string().required(),
+    otherwise: Joi.forbidden(),
+  }),
+  pickup_signing: Joi.alternatives().conditional('status', {
+    is: Joi.equal(status.RECEIVED),
+    then: Joi.string().required(),
+    otherwise: Joi.forbidden(),
+  }),
+  pickup_evidence: Joi.alternatives().conditional('status', {
+    is: Joi.equal(status.RECEIVED),
+    then: Joi.string().required(),
+    otherwise: Joi.forbidden(),
+  }),
+  pickup_bast: Joi.alternatives().conditional('status', {
+    is: Joi.equal(status.RECEIVED),
+    then: Joi.string().required(),
+    otherwise: Joi.forbidden(),
+  }),
 });
