bd3748ef141b86f3b22f11a7ea753d128050795e
iqbal167 <muhammadiqbalali167@gmail.com>
2022-11-15 12:00:38 +0700

    DRY joi when option

--- src/modules/requests/requests.rules.ts
+++ src/modules/requests/requests.rules.ts
@@ -5,6 +5,14 @@ const emptyAllow = ['', null];
 const sortByAllow = ['created_at', 'division', 'status', 'request_type'];
 const sort = ['asc', 'desc'];
 
+const setStatusWhenOption = (status: number[]): Joi.WhenOptions => {
+  return {
+    is: Joi.equal(...status),
+    then: Joi.string().required(),
+    otherwise: Joi.any().strip(),
+  };
+};
+
 export const CreatePayloadSchema = Joi.object({
   division: Joi.string().required(),
   phone_number: Joi.string().required(),
@@ -28,44 +36,36 @@ export const FindAllPayloadSchema = Joi.object({
 
 export const UpdatePayloadSchema = Joi.object({
   status: Joi.number().strict().required(),
-  notes: Joi.alternatives().conditional('status', {
-    is: Joi.equal(status.REJECTED, status.READY),
-    then: Joi.string().required(),
-    otherwise: Joi.any().strip(),
-  }),
-  filename: Joi.alternatives().conditional('status', {
-    is: Joi.equal(status.APPROVED),
-    then: Joi.string().required(),
-    otherwise: Joi.any().strip(),
-  }),
-  item_name: Joi.alternatives().conditional('status', {
-    is: Joi.equal(status.REQUESTED),
-    then: Joi.string().required(),
-    otherwise: Joi.any().strip(),
-  }),
-  item_brand: Joi.alternatives().conditional('status', {
-    is: Joi.equal(status.REQUESTED),
-    then: Joi.string().required(),
-    otherwise: Joi.any().strip(),
-  }),
-  item_number: Joi.alternatives().conditional('status', {
-    is: Joi.equal(status.REQUESTED),
-    then: Joi.string().required(),
-    otherwise: Joi.any().strip(),
-  }),
-  pickup_signing: Joi.alternatives().conditional('status', {
-    is: Joi.equal(status.RECEIVED),
-    then: Joi.string().required(),
-    otherwise: Joi.any().strip(),
-  }),
-  pickup_evidence: Joi.alternatives().conditional('status', {
-    is: Joi.equal(status.RECEIVED),
-    then: Joi.string().required(),
-    otherwise: Joi.any().strip(),
-  }),
-  pickup_bast: Joi.alternatives().conditional('status', {
-    is: Joi.equal(status.RECEIVED),
-    then: Joi.string().required(),
-    otherwise: Joi.any().strip(),
-  }),
+  notes: Joi.alternatives().conditional(
+    'status',
+    setStatusWhenOption([status.REJECTED, status.READY]),
+  ),
+  filename: Joi.alternatives().conditional(
+    'status',
+    setStatusWhenOption([status.APPROVED]),
+  ),
+  item_name: Joi.alternatives().conditional(
+    'status',
+    setStatusWhenOption([status.REQUESTED]),
+  ),
+  item_brand: Joi.alternatives().conditional(
+    'status',
+    setStatusWhenOption([status.REQUESTED]),
+  ),
+  item_number: Joi.alternatives().conditional(
+    'status',
+    setStatusWhenOption([status.REQUESTED]),
+  ),
+  pickup_signing: Joi.alternatives().conditional(
+    'status',
+    setStatusWhenOption([status.RECEIVED]),
+  ),
+  pickup_evidence: Joi.alternatives().conditional(
+    'status',
+    setStatusWhenOption([status.RECEIVED]),
+  ),
+  pickup_bast: Joi.alternatives().conditional(
+    'status',
+    setStatusWhenOption([status.RECEIVED]),
+  ),
 });
