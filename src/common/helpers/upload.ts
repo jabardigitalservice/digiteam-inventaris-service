b115e2015716f5ed534998e03f66f3e8869e55ef
JDS <jds@JDSs-MacBook-Pro-3.local>
2022-12-22 14:04:07 +0700

    refactor: upload validation

--- src/common/helpers/upload.ts
+++ src/common/helpers/upload.ts
@@ -1,14 +1,7 @@
 import { randomUUID } from 'crypto';
-import path, { extname } from 'path';
-import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
-import { FileFilterCallback } from 'multer';
-import { HttpException, HttpStatus } from '@nestjs/common';
-import lang from '../lang/config';
-import { config } from 'dotenv';
+import { extname } from 'path';
 
-config();
-
-const limitSize = parseInt(process.env.FILE_LIMIT_SIZE);
+export const allowExtension = 'jpg|png|pdf|docx|xlsx';
 
 export const createFileObject = (file: Express.Multer.File) => {
   const name = randomUUID();
@@ -25,51 +18,4 @@ export const createFileObject = (file: Express.Multer.File) => {
     fileBuffer,
     metaData,
   };
-};
-
-const allowFileSize = limitSize * 1024 * 1024;
-
-const allowExtension = 'jpeg|png|pdf|docx|xlsx';
-
-const allowMimeTypes = [
-  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-  'application/pdf',
-  'image/png',
-  'image/jpeg',
-];
-
-const formatError = (fieldName: string, message: string): HttpException => {
-  const error: Record<string, string> = {};
-  error[fieldName] = message;
-
-  return new HttpException(error, HttpStatus.UNPROCESSABLE_ENTITY);
-};
-
-const checkFileType = (file: Express.Multer.File, cb: FileFilterCallback) => {
-  const regex = new RegExp(allowExtension);
-  const extname = regex.test(path.extname(file.originalname).toLowerCase());
-
-  const mimeType = allowMimeTypes.includes(file.mimetype);
-
-  if (extname && mimeType) return cb(null, true);
-
-  const customMessage = {
-    attribute: file.fieldname,
-    values: allowExtension.split('|').join(', '),
-  };
-
-  cb(
-    formatError(
-      file.fieldname,
-      lang.__('validation.file.mimetypes', customMessage),
-    ),
-  );
-};
-
-export const multerOptions: MulterOptions = {
-  limits: { fileSize: allowFileSize },
-  fileFilter: (_req, file: Express.Multer.File, cb: FileFilterCallback) => {
-    checkFileType(file, cb);
-  },
 };
